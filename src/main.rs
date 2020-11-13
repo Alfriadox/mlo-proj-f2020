e791ffa87e1b28cf58d127c2a73263d97c3cc0fd
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2020-11-13 03:40:16 -0500

    load first graph

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
-use std::path::PathBuf;
-use std::io::Result as IOResult;
-use sprs::CsMat;
-use petgraph::Graph;
+use std::path::Path;
+use sprs::{CsMat, CsMatI, CsMatBase};
+use petgraph::{Graph, Undirected};
+use csv::{
+    Result as CsvResult,
+    ReaderBuilder
+};
 
-type Scalar = usize;
+/// Load an undirected graph from a path using petgraph.
+/// Will panic if the number of nodes is too low.
+fn load_graph(
+    path: impl AsRef<Path>,
+    delimiter: u8,
+    nodes: usize,
+    has_header_row: bool,
+) -> CsMat<usize> {
+    let mut mat = CsMatI::zero((nodes, nodes));
 
-fn load_graph(path: PathBuf) -> IOResult<CsMat<Scalar>> {
-    let graph = Graph::new_undirected();
+    let mut reader = ReaderBuilder::new()
+        .delimiter(delimiter)
+        .has_headers(has_header_row)
+        .from_path(path.as_ref())
+        .expect("Could not make reader for file");
+
+    for record in reader.deserialize() {
+        let (a, b): (usize, usize) = record.expect("Bad record");
+        // since the graph is undirected, we double set in the matrix.
+        mat.insert(a, b, 1);
+        mat.insert(b, a, 1);
+    }
+
+    println!("Loaded {} to matrix. {} rows, {} non-zero cells.", path.as_ref().display(), mat.rows(), mat.nnz());
+    mat
 }
 
 
 fn main() {
-
+    let twitch_graph = load_graph(
+        "data/twitch/ES/musae_ES_edges.csv",
+        b',',
+        4_648,
+        59_382,
+        true
+    );
 }
