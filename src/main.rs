da005b31ca817bbed98de30f0d11e931ae6228dd
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2020-11-13 02:34:27 -0500

    add twitch data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::PathBuf;
+use std::io::Result as IOResult;
+use sprs::CsMat;
+use petgraph::Graph;
+
+type Scalar = usize;
+
+fn load_graph(path: PathBuf) -> IOResult<CsMat<Scalar>> {
+    let graph = Graph::new_undirected();
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
